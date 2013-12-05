6433210d49a17ab1ae877093d030c3cf577f8d34
Alexander Harkness <bearbin@gmail.com>
2013-12-05 15:08:46 +0000

    Added the most full-featured program yet, lettersinside.

---
+++ lettersInside.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+upper = 0
+
+while True:
+	start = raw_input()[:1]
+	if start.lower() in alphabet:
+		break
+	else:
+		sys.stderr.write("Non-alphabet character entered.")
+if start.isupper():
+	upper += 1
+start = alphabet.find(start.lower())
+
+while True:
+	stop = raw_input()[:1]
+	if stop.lower() in alphabet:
+		break
+	else:
+		sys.stderr.write("Non-alphabet character entered.")
+if stop.isupper():
+	upper += 1
+stop = alphabet.find(stop.lower())
+
+if start > stop:
+	tmp = stop
+	stop = start
+	start = tmp
+
+if upper == 2:
+	alphabet = alphabet.upper()
+
+print(alphabet[start:stop+1])
